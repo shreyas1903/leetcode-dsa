3ec974dc50ce0a3dc61cb1795bc7fe26a1bf1fce
Shreyas Patil <82924523+shreyas1903@users.noreply.github.com>
2021-12-21 16:00:59 +0530

    Create findLucky.java

---
+++ findLucky.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        int[] res = new int[501];
+        for(int i = 0; i< arr.length; i++){
+            res[arr[i]]++;
+        }
+        for(int j = arr.length-1; j>=0; j--){
+            
+            if(j == res[j])
+                return j;
+            
+        }
+        return -1;
+    }
+}
