e9f2b96f0e8ad4fd53149c7f1d2cdcefc769be37
Shreyas Patil <82924523+shreyas1903@users.noreply.github.com>
2021-12-29 12:29:17 +0530

    Create twoSumUsingHash.java

---
+++ twoSumUsingHash.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>(); 
+        for(int i =0 ; i<nums.length; i++){
+            int compliment = target - nums[i];
+            if(map.containsKey(compliment))
+                return new int[] {i,map.get(compliment)};
+            else
+                map.put(nums[i],i);
+        }
+        return new int[] {0,0};
+    }
+}
