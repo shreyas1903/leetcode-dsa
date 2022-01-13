e18e4bd25d927187ebad9e53cd02c597b41a5820
Shreyas Patil <82924523+shreyas1903@users.noreply.github.com>
2022-01-13 23:20:02 +0530

    Create intersection.java

---
+++ intersection.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        return Arrays.stream(nums2).distinct().filter(num->set.contains(num)).toArray();
+    }
+}
