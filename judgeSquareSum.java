267bdefc86926de42256b7b93cfe29d8f3f360a5
Shreyas Patil <82924523+shreyas1903@users.noreply.github.com>
2022-01-17 21:15:36 +0530

    Create judgeSquareSum.java

---
+++ judgeSquareSum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        int left = 0;
+        int right = (int)Math.sqrt(c);
+        while(left <= right){
+            int total = left * left + right * right;
+            if(total == c)
+                return true;
+            else if(total > c)
+                right--;
+            else
+                left++;
+            
+        }
+        return false;
+    }
+}
