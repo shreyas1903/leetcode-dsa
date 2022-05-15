497ffad09a166cf5ababa96b880207dd21594422
Shreyas Patil <82924523+shreyas1903@users.noreply.github.com>
2022-05-16 00:20:23 +0530

    Create minMovesToSeat.java

---
+++ minMovesToSeat.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        int count=0;
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        for(int i =0; i < seats.length; i++){
+             count += Math.abs(seats[i] - students[i]);
+        }
+        return count;
+        
+    }
+}
