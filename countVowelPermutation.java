c5f332c8c5646dd5a3944f21d650a3b72bd1c568
Shreyas Patil <82924523+shreyas1903@users.noreply.github.com>
2022-08-07 20:49:47 +0530

    Create countVowelPermutation.java

---
+++ countVowelPermutation.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    public long pow(int a, int b){
+        long ans=1;
+        for(int i =1;i<=b;i++){
+            ans =ans* a;
+        }
+        return ans;
+    }
+    
+    
+    public int countVowelPermutation(int n) {   
+        
+        long a=1,e=1,i=1,o=1,u=1;
+        long mod = pow(10,9) + 7;
+        long a2,e2,i2,o2,u2;
+        for(int j =2;j<=n;j++){
+            a2 = (e+i+u) % mod;
+            e2 = (a+i) % mod; 
+            i2 = (e+o) % mod;
+            o2 = i;
+            u2 = (o+i) % mod;
+            
+            
+            a=a2;
+            e=e2;
+            i=i2;
+            o=o2;
+            u=u2;
+            
+        }
+        return (int)((a+e+i+o+u)%mod);
+    }
+}          
