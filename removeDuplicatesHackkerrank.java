1619ed8c5a530e761d2f3048499b461adaaf5a43
Shreyas Patil <82924523+shreyas1903@users.noreply.github.com>
2022-08-20 23:36:04 +0530

    Create removeDuplicatesHackkerrank.java

---
+++ removeDuplicatesHackkerrank.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+class Result {
+
+    /*
+     * Complete the 'removeDuplicates' function below.
+     *
+     * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+     * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+     */
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+
+    public static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode llist) {
+        HashSet<Integer> res = new HashSet<>();
+        SinglyLinkedListNode curr = llist.next;
+        SinglyLinkedListNode prev = null;
+        
+        while(curr != null){
+            int val = curr.data;
+            if(res.contains(val)){
+                prev.next = curr.next;
+                
+            }
+            else{
+                res.add(val);
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return llist;
+    }
+
+}
+
+    private static final Scanner scanner = new Scanner(System.in);
