23abb5fa3cc845a96286985bbd9036aac2e12089
Shreyas Patil <82924523+shreyas1903@users.noreply.github.com>
2022-11-29 19:46:15 +0530

    Create swapPairsiterative.java

---
+++ swapPairsiterative.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode node = dummy;
+        while(node != null){
+            ListNode first = node.next;
+            ListNode second = null;
+            if(first != null){
+                second = first.next;
+            }
+
+            if(second != null){
+                ListNode secondnext = second.next;
+                second.next = first;
+                node.next = second;
+                first.next = secondnext;
+                node = first;
+            }
+            else{
+                break;
+            }
+        }
+        return dummy.next;
+    }
+    // public void swap(ListNode node){
+    //     if(node == null){
+    //         return ;
+    //     }
+        
+        
+    // }
+}
