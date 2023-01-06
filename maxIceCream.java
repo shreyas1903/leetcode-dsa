36037df184b0cec24da1a34577dcabf31633482b
Shreyas Patil <82924523+shreyas1903@users.noreply.github.com>
2023-01-06 17:47:13 +0530

    Create maxIceCream.java

---
+++ maxIceCream.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count =0;
+        for(int i =0;i<costs.length;i++){
+            if(costs[i] <= coins){
+                
+                coins -= costs[i];
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
